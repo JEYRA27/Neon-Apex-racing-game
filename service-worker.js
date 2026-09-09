9cf44671d8cc0c53b2b1fbd21becd0db338014ce
JEYASEELAN RA <2k22cse061@kiot.ac.in>
2026-09-09 13:21:29 +0530

    Add mobile app file: service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = 'neon-apex-v1';
+const ASSETS = ['./','./index.html','./styles.css','./game.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    if (new URL(event.request.url).origin === location.origin) caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
+
